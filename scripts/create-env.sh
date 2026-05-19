4a8c365ed559368eec77dac3d620c2a8b7d7ac22
LapTQ <lap.tq4@gmail.com>
2026-05-19 11:37:44 +0700

    .

---
+++ scripts/create-env.sh
@@ -0,0 +1,24 @@
+DIR_PRJ=.
+VENV_PARENT=/mnt/hdd10tb/Users/laptq/rf-detr
+# VENV_PARENT=.
+
+VENV_NAME=.venv
+VENV_PATH=$VENV_PARENT/$VENV_NAME
+[[ ! -d $VENV_PATH ]] && python3 -m venv $VENV_PATH
+
+if [ $( realpath "$VENV_PARENT" ) != $( realpath "$DIR_PRJ" ) ]; then
+    ln -sf $VENV_PATH $DIR_PRJ/
+fi
+
+source $DIR_PRJ/$VENV_NAME/bin/activate
+which python3
+
+
+# git config user.name "LapTQ"
+# git config user.email "lap.tq4@gmail.com"
+
+pip install -e .[train,loggers]
+
+# rack
+pip uninstall -y torch torchvision
+pip install torch torchvision --index-url https://download.pytorch.org/whl/cu121
